9ea2a35efdf2cdd1cda2f764198a604bd0b1ce5f
manuelbuehler <manuel.buehler@edu.gbssg.ch>
2024-06-25 15:26:25 +0200

    Initial Commit

---
+++ GuideMeApp/GuideMeApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GuideMeApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ GuideMeApp/GuideMeApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GuideMeApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
