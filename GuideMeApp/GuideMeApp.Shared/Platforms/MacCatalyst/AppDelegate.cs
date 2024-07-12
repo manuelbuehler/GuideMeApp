d640d3cda37cfbb94423a52ad162d82d467c5f84
manuelbuehler <manuel.buehler@edu.gbssg.ch>
2024-07-12 10:17:59 +0200

    Init shared

---
+++ GuideMeApp/GuideMeApp.Shared/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GuideMeApp.Shared
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
