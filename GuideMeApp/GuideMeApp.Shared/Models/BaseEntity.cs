bcf8e978f45fbb0a18b4cc8bbcbc06bbac545585
manuelbuehler <manuel.buehler@edu.gbssg.ch>
2024-07-12 13:53:23 +0200

    - changed shared

--- GuideMeApp/GuideMeApp.Shared/Models/BaseEntity.cs
+++ GuideMeApp/GuideMeApp.Shared/Models/BaseEntity.cs
@@ -1,12 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GuideMeApp.Shared.Models
 {
     public abstract class BaseEntity
     {
         [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
+
+        public BaseEntity()
+        {
+            Id = Guid.NewGuid();
+        }
     }
 }
